d761d9e5058d923184d89a120f58d863aa7ffff5
Afifah Miftakhul <apipiyahmiftakhul@gmail.com>
2025-05-13 15:55:27 +0700

    memanggil constructor

--- constructorBerparameter.cpp
+++ constructorBerparameter.cpp
@@ -7,3 +7,14 @@ class mahasiswa {
     mahasiswa(int nim, string nama); // Constructor dengan parameter
 };
 
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama){
+    cout << "Constructor Parameter Terpanggil" << endl;
+    cout << "NIM : " << endl;
+    cout << "Nama : " << endl;
+}
+
+int main(){
+    mahasiswa(12345, "Pascal");    // Memanggil Constructor dengan parameter
+    return 0;
+}
