c588fb5189fa5804e26de518c04a2a878e8ef5e0
Afifah Miftakhul <apipiyahmiftakhul@gmail.com>
2025-05-13 16:23:41 +0700

    this

---
+++ this.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+
+    public :
+    string setget(string judul)
+    {
+    this->judul = judul; //nilai parameter 'judul' untuk member variabel 'judul'
+    return this->judul; 
+    }
+};
+
+int main(){
+    buku bukunya;
+    cout<< bukunya.setget("Matematika");
+    return 0;
+}
